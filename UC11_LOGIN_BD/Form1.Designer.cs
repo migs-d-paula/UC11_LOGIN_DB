99658e423dd3c43720ac62bfde33187911a99c73
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-04-25 22:16:16 -0300

    Adicionar arquivos de projeto.

---
+++ UC11_LOGIN_BD/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UC11_LOGIN_BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUSUARIO = new System.Windows.Forms.TextBox();
+            this.textBoxSENHA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonENTRAR = new System.Windows.Forms.Button();
+            this.buttonLIMPAR = new System.Windows.Forms.Button();
+            this.buttonCADASTRAR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUSUARIO
+            // 
+            this.textBoxUSUARIO.Location = new System.Drawing.Point(133, 85);
+            this.textBoxUSUARIO.Name = "textBoxUSUARIO";
+            this.textBoxUSUARIO.Size = new System.Drawing.Size(499, 20);
+            this.textBoxUSUARIO.TabIndex = 0;
+            // 
+            // textBoxSENHA
+            // 
+            this.textBoxSENHA.Location = new System.Drawing.Point(133, 167);
+            this.textBoxSENHA.Name = "textBoxSENHA";
+            this.textBoxSENHA.Size = new System.Drawing.Size(499, 20);
+            this.textBoxSENHA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "senha";
+            // 
+            // buttonENTRAR
+            // 
+            this.buttonENTRAR.Location = new System.Drawing.Point(232, 231);
+            this.buttonENTRAR.Name = "buttonENTRAR";
+            this.buttonENTRAR.Size = new System.Drawing.Size(84, 23);
+            this.buttonENTRAR.TabIndex = 4;
+            this.buttonENTRAR.Text = "Entrar";
+            this.buttonENTRAR.UseVisualStyleBackColor = true;
+            this.buttonENTRAR.Click += new System.EventHandler(this.buttonENTRAR_Click);
+            // 
+            // buttonLIMPAR
+            // 
+            this.buttonLIMPAR.Location = new System.Drawing.Point(469, 231);
+            this.buttonLIMPAR.Name = "buttonLIMPAR";
+            this.buttonLIMPAR.Size = new System.Drawing.Size(77, 23);
+            this.buttonLIMPAR.TabIndex = 5;
+            this.buttonLIMPAR.Text = "Limpar";
+            this.buttonLIMPAR.UseVisualStyleBackColor = true;
+            // 
+            // buttonCADASTRAR
+            // 
+            this.buttonCADASTRAR.Location = new System.Drawing.Point(198, 300);
+            this.buttonCADASTRAR.Name = "buttonCADASTRAR";
+            this.buttonCADASTRAR.Size = new System.Drawing.Size(373, 23);
+            this.buttonCADASTRAR.TabIndex = 6;
+            this.buttonCADASTRAR.Text = "Cadastrar-se";
+            this.buttonCADASTRAR.UseVisualStyleBackColor = true;
+            this.buttonCADASTRAR.Click += new System.EventHandler(this.buttonCADASTRAR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCADASTRAR);
+            this.Controls.Add(this.buttonLIMPAR);
+            this.Controls.Add(this.buttonENTRAR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSENHA);
+            this.Controls.Add(this.textBoxUSUARIO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUSUARIO;
+        private System.Windows.Forms.TextBox textBoxSENHA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonENTRAR;
+        private System.Windows.Forms.Button buttonLIMPAR;
+        private System.Windows.Forms.Button buttonCADASTRAR;
+    }
+}
+
---
+++ UC11_LOGIN_BD/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC11_LOGIN_BD
+{
+    public partial class Form1 : Form
+    {
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+
+        public Form1()
+        {
+            InitializeComponent();
+            servidor = "Server=localhost;Database=login_db;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+        }
+
+        private void buttonENTRAR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCADASTRAR_Click(object sender, EventArgs e)
+        {
+            conexao.Open();
+            comando.CommandText = "INSERT INTO tbl_usuarios(usuario, senha) VALUES ('" + textBoxUSUARIO.Text + "', '" + textBoxSENHA.Text + "');" ;
+
+            try
+            {
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
